6278f3c58d5bbfc4055e4e97ba4ba6963ae07e57
admin <admin@Bcalab3-m05>
2022-03-09 10:52:04 +0530

    COMMIT1

---
+++ Githublab/src/Employee.java
@@ -0,0 +1,12 @@
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Employee Name: Diraj Naik");
+		System.out.println("Employee Dept: BCA");
+		
+
+	}
+
+}
