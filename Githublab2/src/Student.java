20299ef1d318fe193f9d618343f6f6eb642ec67e
admin <admin@Bcalab3-m05>
2022-03-09 09:07:47 +0530

    commit2

--- Githublab2/src/Student.java
+++ Githublab2/src/Student.java
@@ -7,6 +7,7 @@ public class Student {
 		System.out.println("Student Address: Navelim");
 		System.out.println("Student Rollno: R/BCA-20-108");
 		System.out.println("Student Stream: BCA");
+		System.out.println("Student DOB: 20th March 2022");
 		
 		
 
