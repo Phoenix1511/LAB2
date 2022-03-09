4c468ae75a0961477ed357bb9591b19fcd186c84
admin <admin@Bcalab3-m05>
2022-03-09 10:36:05 +0530

    COMMIT2

---
+++ Githublab2/src/Grades.java
@@ -0,0 +1,20 @@
+
+public class Grades {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Student_1 Name: Fayaz");
+		System.out.println("Student_1 Overall Grade: B");
+		System.out.println("\n");
+		System.out.println("Student_2 Name: Diraj");
+		System.out.println("Student_2 Overall Grade: A");
+		System.out.println("\n");
+		System.out.println("Student_3 Name: Jolvin");
+		System.out.println("Student_3 Overall Grade: A");
+		
+		
+		
+
+	}
+
+}
