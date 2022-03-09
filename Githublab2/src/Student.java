2ddaa081d3bf01cc581d980f4be95d057eb40ee2
admin <admin@Bcalab3-m05>
2022-03-09 09:04:24 +0530

    COMMIT1

---
+++ Githublab2/src/Student.java
@@ -0,0 +1,15 @@
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Student Name: Diraj Naik");
+		System.out.println("Student Address: Navelim");
+		System.out.println("Student Rollno: R/BCA-20-108");
+		System.out.println("Student Stream: BCA");
+		
+		
+
+	}
+
+}
