0787ded18c8ce1d170d718bffbe32cc6bf8c4d72
admin <admin@Bcalab3-m05>
2022-03-16 10:16:51 +0530

    commit

--- Githublab/src/Employee.java
+++ Githublab/src/Employee.java
@@ -1,10 +1,24 @@
 
 public class Employee {
+	
+	String name,dpt;
+	
+	Employee(String name,String dpt)
+	{
+		this.name=name;
+		this.dpt=dpt;
+	}
+	
+	void display()
+	{
+		System.out.println("Employee Name: "+name);
+		System.out.println("Employee Department: "+dpt);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Employee Name: Diraj Naik");
-		System.out.println("Employee Dept: BCA");
+		Employee e = new Employee("Fayaz","BCA");
+		e.display();
 		
 
 	}
